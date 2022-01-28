dd64cd3f539c7a24079cee9888988e5f039eb7b4
Len <LenAnderson@users.noreply.github.com>
2022-01-28 07:50:55 +0100

    Show empty profile pictures for comments without author
    This fixes alignment of comments for [deleted] and other instances where there is no author or profile picture.

--- reddit_profile_pictures.user.js
+++ reddit_profile_pictures.user.js
@@ -2,7 +2,7 @@
 // @name         Reddit - Profile Pictures
 // @namespace    https://github.com/LenAnderson/
 // @downloadURL  https://github.com/LenAnderson/Reddit-Profile-Pictures/raw/master/reddit_profile_pictures.user.js
-// @version      1.1
+// @version      1.2
 // @description  Show profile pictures on reddit.
 // @author       LenAnderson
 // @match        https://www.reddit.com/r/*/comments/*
@@ -13,28 +13,30 @@
     'use strict';
 
     const addAvatars = async(root=document)=>{
-		Array.from(root.querySelectorAll('.author')).forEach(async(a)=>{
-			const thing = a.closest('.thing');
+		Array.from(root.querySelectorAll('.thing')).forEach(async(thing)=>{
+			const a = thing.querySelector(`#${thing.id} > .entry > .tagline > .author`);
 			if (!thing) return;
 			if (thing.hasAttribute('data-reddit-profile-picture')) return;
-			const xhr = new XMLHttpRequest();
-			xhr.open('GET', `${a.href}/about.json`);
-			xhr.addEventListener('load', async()=>{
-				if (thing.hasAttribute('data-reddit-profile-picture')) return;
-				const profile = JSON.parse(xhr.responseText).data;
-				const img = document.createElement('img'); {
+			const img = document.createElement('img'); {
+				img.classList.add('reddit-profile-picture');
+				img.style.height = '48px';
+				img.style.width = '48px';
+				img.style.float = 'left';
+				img.style.marginRight = '7px';
+				thing.insertBefore(img, thing.querySelector('.entry'));
+			}
+			thing.setAttribute('data-reddit-profile-picture', 1);
+			if (a && a.href) {
+				const xhr = new XMLHttpRequest();
+				xhr.open('GET', `${a.href}/about.json`);
+				xhr.addEventListener('load', async()=>{
+					const profile = JSON.parse(xhr.responseText).data;
 					const ta = document.createElement('textarea');
 					ta.innerHTML = profile.icon_img;
-					img.classList.add('reddit-profile-picture');
 					img.src = ta.value;
-					img.style.height = '48px';
-					img.style.float = 'left';
-					img.style.marginRight = '7px';
-					thing.insertBefore(img, thing.querySelector('.entry'));
-				}
-				thing.setAttribute('data-reddit-profile-picture', 1)
-			});
-			xhr.send();
+				});
+				xhr.send();
+			}
 		});
 	};
 	addAvatars();
